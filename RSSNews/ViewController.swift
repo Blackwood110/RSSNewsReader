71b203a9b890109751082666b5c72677128ebd93
Сашка <aleksandrdergelev@mail.ru>
2019-10-11 20:03:38 +0300

    Initial Commit

---
+++ RSSNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSNews
+//
+//  Created by Александр Дергилёв on 11/10/2019.
+//  Copyright © 2019 Александр Дергилёв. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
